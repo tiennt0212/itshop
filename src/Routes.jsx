a58f59d64c49095684619460ce7bad520aab4cee
Nguyen Thanh Tien <tiennt0212@gmail.com>
2021-10-04 09:57:08 +0700

    :zap: add Route

---
+++ src/Routes.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ROUTES } from './utils/routeConstants';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+        <Switch>
+            
+        </Switch>
+        </BrowserRouter>
+    )
+}
---
+++ src/utils/routeConstants.js
@@ -0,0 +1,5 @@
+export const ROUTES = {
+    TRANGCHU: '/',
+    DANGNHAP: '/dang-nhap',
+    SANPHAM: '/san-pham',
+}
