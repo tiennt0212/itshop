6b91dab5b973b4d935762127aba104ecce3ebb06
Nguyen Thanh Tien <tiennt0212@gmail.com>
2021-10-15 23:56:00 +0700

    :rocket: first test

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ROUTES } from './utils/routeConstants';
-
+// import component Home, About Us,...
+import Home from './containers/Home';
 const Routes = () => {
     return (
         <BrowserRouter>
-        <Switch>
-            
-        </Switch>
+            <Switch>
+                <Route path='/' exact component={Home}/>
+                <Route path='/abc' exact component={Home}/>
+            </Switch>
         </BrowserRouter>
     )
-}
+};
+
+export default Routes;
